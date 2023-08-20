178a38f2335c2a1d8b84e19642ae436ec0376af3
Luke <luket8032@hotmail.com>
2023-08-20 16:54:53 -0500

    Attempt delete node fn (not working)

--- bst.js
+++ bst.js
@@ -38,6 +38,53 @@ const Tree = (inputArr) => {
         return root;
     };
 
+    const deleteNode = (data) => {
+        bst = deleteRec(bst, data);
+    }
+
+    const deleteRec = (root, data) => {
+        if(root.data === null) {
+            return root;
+        }
+
+        if(root.data > data) {
+            deleteRec(root.left, data);
+        } else if (root.data < data) {
+            deleteRec(root.right, data);
+        }
+
+        if (root.left === null) {
+            let temp = root.right;
+            delete root;
+            return temp;
+        } else if (root.right === null) {
+            let temp = root.left;
+            delete root;
+            return temp;
+        }
+
+        else {
+            let succParent = root;
+
+            let succ = root.right;
+            while (succ.left !== null) {
+                succParent = succ;
+                succ = succ.left;
+            }
+
+            if (succParent !== root) {
+                succParent.left = succ.right;
+            } else {
+                succParent.right = succ.right;
+            }
+
+            root.key = succ.key;
+
+            delete succ;
+            return root;
+        }
+    }
+
     const prettyPrint = (node, prefix = "", isLeft = true) => {
         if (node === null) {
           return;
@@ -53,17 +100,18 @@ const Tree = (inputArr) => {
 
     bst = buildTree(inputArr, 0, inputArr.length - 1);
 
-    return { prettyPrint, insert, bst };
+    return { prettyPrint, insert, deleteNode, bst };
 }
 
 const test = [40, 100, 1, 5, 25, 10];
 
 const testTree = Tree(test);
 
-testTree.insert(3);
 testTree.insert(40);
 testTree.insert(50);
 testTree.insert(60);
 testTree.insert(110);
+
+testTree.deleteNode(100);
 
 console.log(testTree.prettyPrint(testTree.bst));
