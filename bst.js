d948e3484e9f37da72f1be15fdd3c60ea1e5b853
Luke <luket8032@hotmail.com>
2023-08-20 13:33:48 -0500

    Create sort function for arrays

--- bst.js
+++ bst.js
@@ -1,3 +1,25 @@
 const Node = (data, left = null, right = null) => {
     return {data, left, right}
 }
+
+const Tree = (inputArr) => {
+    const sortArr = (arr) => {
+        let sorted = new Set(arr);
+    }
+
+    const buildTree = (arr, start, end) => {
+        if(start > end) return null;
+    }
+
+    return { buildTree }
+}
+
+const sortArr = (arr) => {
+    let sorted = [...new Set(arr)];
+    sorted = sorted.sort(function(a, b){return a-b});
+    return sorted
+}
+
+const test = [40, 100, 1, 5, 4, 4, 25, 10]
+
+console.log(sortArr(test));
